de0f13672d6a5f151ed7eaca6a3c0818903d570d
dlsxo1023 <149138829+dlsxo1023@users.noreply.github.com>
2024-02-13 16:51:38 +0900

    Add files via upload

---
+++ OpenCV C++/cpp/CH7/CH7/HelloCV.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+void filter_embossing();
+
+int main()
+{
+    filter_embossing();
+
+    return 0;
+}
+
+void filter_embossing()
+{
+    Mat img = imread("../../images/flower1.jpg");
+    if (img.empty())
+    {
+        cerr << "Image load fail" << endl;
+        return;
+    }
+
+    float data[] = {
+        -1, -1, 0,
+        -1, 0, 1,
+         0, 1, 1
+    };
+
+    Mat emboss(3, 3, CV_32FC1, data);
+    Mat dst;
+    filter2D(img, dst, -1, emboss);
+
+    imshow("image", img);
+    imshow("dst", dst);
+    waitKey();
+    destroyAllWindows();
+}
---
+++ OpenCV C++/cpp/CH7/blurr/HelloCV.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+void blurring_mean();
+void blurring_gaussian();
+void sharpen();
+
+int main()
+{
+    //blurring_mean();
+    //blurring_gaussian();
+    sharpen();
+
+    return 0;
+}
+
+void blurring_mean()
+{
+    Mat img = imread("../../images/lena.jpg");
+    imshow("image", img);
+
+    Mat dst;
+    for (size_t i = 1; i < 10; i++)
+    {
+        blur(img, dst, Size(i, i));
+        imshow("blur", dst);
+        waitKey(500);
+    }
+    waitKey();
+    destroyAllWindows();
+}
+
+void blurring_gaussian()
+{
+    Mat img = imread("../../images/lena.jpg");
+    imshow("image", img);
+
+    Mat dst;
+    for (size_t i = 1; i < 10; i++)
+    {
+        GaussianBlur(img, dst, Size(0, 0), (double)(i));
+        imshow("blur", dst);
+        waitKey(500);
+    }
+    waitKey();
+    destroyAllWindows();
+}
+
+void sharpen()
+{
+    Mat src = imread("../../images/lena.jpg");
+    imshow("image", src);
+
+    for (size_t i = 1; i < 10; i++)
+    {
+        Mat blurred;
+        GaussianBlur(src, blurred, Size(), i);
+
+        float alpha = 1.f;
+        Mat dst = src + (alpha * src - alpha * blurred);
+
+        imshow("blur", blurred);
+        imshow("dst", dst);
+        waitKey(500);
+    }
+    waitKey();
+    destroyAllWindows();
+}
